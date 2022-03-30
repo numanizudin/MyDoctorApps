9fdf5681793c780829bb418912876e5d83531a79
numanizudin <muhammadnumanizudin@gmail.com>
2022-03-30 09:41:23 +0700

    Home Profile

---
+++ src/components/molecules/HomeProfile/index.js
@@ -0,0 +1,33 @@
+import {Image, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import React from 'react';
+import {colors, fonts} from '../../../utils';
+import {DummyUser} from '../../../assets';
+
+export default function HomeProfile() {
+  return (
+    <View style={styles.container}>
+      <Image source={DummyUser} style={styles.avatar} />
+      <View>
+        <Text style={styles.name}>Anya Geraldine</Text>
+        <Text style={styles.profession}>Programmer</Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {flexDirection: 'row'},
+  avatar: {width: 46, height: 46, borderRadius: 46 / 2, marginRight: 12},
+  name: {
+    fontSize: 16,
+    fontFamily: fonts.primary[600],
+    color: colors.text.primary,
+    textTransform: 'capitalize',
+  },
+  profession: {
+    fontSize: 12,
+    fontFamily: fonts.primary[400],
+    color: colors.text.secondary,
+    textTransform: 'capitalize',
+  },
+});

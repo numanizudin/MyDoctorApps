51b24f302cd9b745d0915a3294bda8f02e16f856
numanizudin <muhammadnumanizudin@gmail.com>
2022-03-30 11:22:02 +0700

    Page Hospitals

--- src/pages/Hospitals/index.js
+++ src/pages/Hospitals/index.js
@@ -1,12 +1,52 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View, ImageBackground} from 'react-native';
 import React from 'react';
+import {ILHospitalBG} from '../../assets/illustration';
+import {colors, fonts} from '../../utils';
+import {ListHospitals} from '../../components';
 
 export default function Hospitals() {
   return (
-    <View>
-      <Text>Hospitals</Text>
+    <View style={styles.page}>
+      <ImageBackground source={ILHospitalBG} style={styles.background}>
+        <Text style={styles.title}>Nearby Hospitals</Text>
+        <Text style={styles.desc}>3 Tersedia</Text>
+      </ImageBackground>
+      <View style={styles.content}>
+        <ListHospitals />
+        <ListHospitals />
+        <ListHospitals />
+      </View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  page: {
+    backgroundColor: colors.primary,
+    flex: 1,
+  },
+  background: {
+    height: 240,
+    paddingTop: 30,
+  },
+  title: {
+    fontSize: 20,
+    fontFamily: fonts.primary[600],
+    color: colors.white,
+    textAlign: 'center',
+  },
+  desc: {
+    fontSize: 14,
+    fontFamily: fonts.primary[300],
+    color: colors.white,
+    textAlign: 'center',
+    marginTop: 6,
+  },
+  content: {
+    backgroundColor: colors.white,
+    flex: 1,
+    borderRadius: 20,
+    marginTop: -30,
+    paddingTop: 14,
+  },
+});

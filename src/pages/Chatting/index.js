49a4d67b2a33893b2a1fd30b2e4e8f93537ea4c6
numanizudin <muhammadnumanizudin@gmail.com>
2022-04-02 09:25:57 +0700

    mengirim data chatting ke firebase

--- src/pages/Chatting/index.js
+++ src/pages/Chatting/index.js
@@ -1,10 +1,50 @@
 import {StyleSheet, Text, View, ScrollView} from 'react-native';
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {ChatItem, Header, InputChat} from '../../components';
-import {colors, fonts} from '../../utils';
+import {colors, fonts, getData, showError} from '../../utils';
+import {Fire} from '../../config';
 
 export default function Chatting({navigation, route}) {
   const dataDoctor = route.params;
+  const [chatContent, setChatContent] = useState('');
+  const [user, setUser] = useState({});
+
+  useEffect(() => {
+    getData('user').then(res => {
+      setUser(res);
+    });
+  }, []);
+
+  const chatSend = () => {
+    const today = new Date();
+    const hour = today.getHours();
+    const minute = today.getMinutes();
+    const year = today.getFullYear();
+    const month = today.getMonth() + 1;
+    const date = today.getDate();
+
+    const data = {
+      sendBy: user.uid,
+      chatDate: new Date().getTime(),
+      chatTIme: `${hour}:${minute} ${hour > 12 ? 'PM' : 'AM'}`,
+      chatContent: chatContent,
+    };
+    setChatContent('');
+    Fire.database()
+      .ref(
+        `chatting/${user.uid}_${
+          dataDoctor.data.uid
+        }/allChat/${year}-${month}-${date}`,
+      )
+      .push(data)
+      .then(() => {
+        setChatContent('');
+      })
+      .catch(err => {
+        showError(err.message);
+      });
+  };
+
   return (
     <View style={styles.page}>
       <Header
@@ -23,9 +63,9 @@ export default function Chatting({navigation, route}) {
         </ScrollView>
       </View>
       <InputChat
-        value=""
-        onChangeText={() => alert('Input Tap')}
-        onButtonPress={() => alert('input button')}
+        value={chatContent}
+        onChangeText={value => setChatContent(value)}
+        onButtonPress={chatSend}
       />
     </View>
   );

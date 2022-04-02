14fac1ad363bea493752d89aabd1f427483d4e61
numanizudin <muhammadnumanizudin@gmail.com>
2022-04-02 10:25:00 +0700

    pengambilan data chatting dari doctor

--- src/components/molecules/ChatItem/index.js
+++ src/components/molecules/ChatItem/index.js
@@ -4,9 +4,9 @@ import {colors, fonts} from '../../../utils';
 import IsMe from './IsMe';
 import Other from './Other';
 
-export default function ChatItem({isMe, text, date}) {
+export default function ChatItem({isMe, text, date, photo}) {
   if (isMe) {
     return <IsMe text={text} date={date} />;
   }
-  return <Other />;
+  return <Other text={text} date={date} photo={photo} />;
 }
